60ec9d37fbe05bb7ae5ef5d2e4e4e5922a7da260
matthewdozeman <112421220+matthewdozeman@users.noreply.github.com>
2023-01-24 18:51:36 +0000

    aoeu

---
+++ math.cs
@@ -0,0 +1,214 @@
+namespace Calculator
+{
+    public static class MyCalculator
+    {
+    public static double Add(int x, int y)
+    {
+        return x + y;
+    }
+    public static double Add(float x, float y)
+    {
+        return x + y;
+    }
+    public static double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+
+    public static double Subtract(int x, int y)
+    {
+        return x - y;
+    }
+    public static double Subtract(float x, float y)
+    {
+        return x - y;
+    }
+    public static double Subtract(double x, double y)
+    {
+        return x - y;
+    }
+
+
+    public static double Multiply(int x, int y)
+    {
+        return x * y;
+    }    
+    public static double Multiply(float x, float y)
+        {
+            return x * y;
+        }    
+    public static double Multiply(double x, double y)
+    {
+        return x * y;
+    }    
+
+
+    public static double Divide(int x, int y)
+    {
+        return x / y;
+    }
+    public static double Divide(float x, float y)
+    {
+        return x / y;
+    }  
+    public static double Divide(double x, double y)
+    {
+        return x / y;
+    }  
+
+
+    public static double Pow(int x, int y)
+    {
+        return Math.Pow(x, y);
+    }
+    public static double Pow(float x, float y)
+    {
+        return Math.Pow(x, y);
+    }
+    public static double Pow(double x, double y)
+    {
+        return Math.Pow(x, y);
+    }
+
+
+    public static double Sqrt(int a)
+    {
+        double ans = Math.Sqrt(a);
+        return ans;
+    }
+    public static double Sqrt(float a)
+    {
+        double ans = Math.Sqrt(a);
+        return ans;
+    }
+    public static double Sqrt(double a)
+    {
+        double ans = Math.Sqrt(a);
+        return ans;
+    }
+
+
+    public static double Min(int a, int b)
+    {
+        double ans = Math.Min(a, b);
+        return ans;
+    }
+    public static double Min(float a, float b)
+    {
+        double ans = Math.Min(a, b);
+        return ans;
+    }
+    public static double Min(double a, double b)
+    {
+        double ans = Math.Min(a, b);
+        return ans;
+    }
+
+
+    public static double Max(int a, int b)
+    {
+        double ans = Math.Max(a, b);
+        return ans;
+    }
+    public static double Max(float a, float b)
+    {
+        double ans = Math.Max(a, b);
+        return ans;
+    }
+    public static double Max(double a, double b)
+    {
+        double ans = Math.Max(a, b);
+        return ans;
+    }
+
+    public static double Ceiling(decimal a)
+    {
+        double ans = Math.Ceiling(a);
+        return ans;
+    }
+    public static double Ceiling(float a)
+    {
+        double ans = Math.Ceiling(a);
+        return ans;
+    }
+    public static double Ceiling(double a)
+    {
+        double ans = Math.Ceiling(a);
+        return ans;
+    }
+
+
+    public static double Floor(float a)
+    {
+        double ans = Math.Floor(a);
+        return ans;
+    }
+    public static double Floor(double a)
+    {
+        double ans = Math.Floor(a);
+        return ans;
+    }
+    public static double Floor(decimal a)
+    {
+        double ans = Math.Floor(a);
+        return ans;
+    }
+    
+
+
+    public static double Abs(int a)
+    {
+        double ans = Math.Abs(a);
+        return ans;
+    }
+    public static double Abs(float a)
+    {
+        double ans = Math.Abs(a);
+        return ans;
+    }
+    public static double Abs(double a)
+    {
+        double ans = Math.Abs(a);
+        return ans;
+    }
+
+
+    public static double Round(float a)
+    {
+        double ans = Math.Round(a);
+        return ans;
+    }
+    public static double Round(decimal a)
+    {
+        double ans = Math.Round(a);
+        return ans;
+    }
+    public static double Round(double a)
+    {
+        double ans = Math.Round(a);
+        return ans;
+    }
+
+
+    public static double Round(float a, int digits)
+    {
+        double ans = Math.Round(a, digits);
+        return ans;
+    }
+    public static double Round(double a, int digits)
+    {
+        double ans = Math.Round(a, digits);
+        return ans;
+    }
+    public static double Round(decimal a, int digits)
+    {
+        double ans = Math.Round(a, digits);
+        return ans;
+    }
+}
+
+
+
+
+
