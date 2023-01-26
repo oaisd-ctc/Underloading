8dbbaeaef2c44ee3190a7d759b1d0bf99c2dc328
Lukas Batema <112420897+lukas-batema-student@users.noreply.github.com>
2023-01-26 19:20:48 +0000

    "Alright guys, [cough] I'm doing 'Take On Me' now, alright. I, I really, I really like this song, it sounds good."
    Minin' away
    I don't know what to mine
    I'll mine this anyway
    In this Minecraft day
    So beautiful, mine further down
    What's that I found?
    Mine diamonds (Take on me)
    Mine diamonds (Take on me)
    I'll mine them
    So far I've got two!
    So easy to mine
    With my Minecraft pickaxe and shovels
    Hopefully they stay
    In my Minecraft chests
    So I'm gonna make
    A lock on it
    Mine diamonds (Take on me)
    Mine diamonds (Take on me)
    I'll mine them
    So far I've got two!
    "Ah! [cough, cough, cough, cough, sniff, cough]
    I'm alright, I'm ready"
    All these diamonds
    Sittin' carefully lay
    I'm getting worried ("Shut the fucking door!")
    If they might get stolen
    From my ender chest
    Wait, who is that?
    Holy sheep, it's Notch!
    Mine diamonds (Take on me)
    Mine diamonds (Take on me)
    Now they're safe
    Woahhhhhhhhhhhhhhhhh!
    Now...
    Now that they're safe
    Woahhhhhhhhhhhhhhhhh!
    Mine diamonds, [heavy breathing] (Take on me)
    Mine diamonds (Take on me)
    "Uh, Thanks, thanks for listening guys and thanks for recommending this song"

--- Underloading/StringUtils/HasLetter.cs
+++ Underloading/StringUtils/HasLetter.cs
@@ -1,5 +1,7 @@
-namespace StringUtils {
-    public static class HasLetter {
+namespace StringUtils
+{
+    public static class HasLetter
+    {
         public static string IncludesLetter()
         {
             Console.WriteLine("HasLetter");
@@ -11,43 +13,69 @@ namespace StringUtils {
             string letterInInput = "";
             string letterFound = "{ ";
             string returnQuery = "";
+            char prevLetter = '℗';
 
-            try {
+            try
+            {
                 char letterSearch = UserInputChar();
 
                 int i = 0;
 
-                foreach(char letter in input)
+                foreach (char letter in input)
                 {
-                    if (i < input.Length)
+                    if ((i < input.Length) && (i != (input.Length - 1)))
                     {
-                        if (letter == letterSearch && i == 0)
+                        if (((letter == letterSearch) && (i == 0)) || ((prevLetter != letterSearch) && (letter == letterSearch) && (i - 1 == 0)))
                         {
                             letterFound += $"{i}";
 
                             letterInInput = "true";
                         }
-                        else if (letter == letterSearch && i > 0)
+                        else if ((((prevLetter == letterSearch) && (letter == letterSearch)) || ((prevLetter == letterSearch) && (letter == letterSearch)) || ((prevLetter != letterSearch) && (letter == letterSearch))) && (i > 0))
                         {
                             letterFound += $", {i}";
+
+                            letterInInput = "true";
                         }
-                    } else if (i == input.Length) {
+                    }
+                    else if (i == (input.Length - 1))
+                    {
+                        if ((letter == letterSearch && i == 0) || ((prevLetter != letterSearch) && (letter == letterSearch && i > 0)))
+                        {
+                            letterFound += $"{i}";
+
+                            letterInInput = "true";
+                        }
+                        else if ((prevLetter == letterSearch) && (letter == letterSearch))
+                        {
+                            letterFound += $", {i}";
+
+                            letterInInput = "true";
+                        }
+                        else if ((letter == letterSearch) && (prevLetter == letterSearch))
+                        {
+                            letterFound += $"{i}";
+                        }
+                        else if ((letter != letterSearch) && (prevLetter != letterSearch))
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
                         if (letter == letterSearch)
                         {
                             letterFound += $", {i} }}";
                         }
                         else if (letter != letterSearch && letterFound.Length == 2)
                         {
-                            letterFound = "{ -1";
+                            letterFound += "-1 }";
+
                             letterInInput = "false";
                         }
                         else if (letter != letterSearch && letterFound.Length > 2)
                         {
                             letterFound += " }";
-                        }
-                        else
-                        {
-                            letterFound += $", {i} }}";
                         }
                     }
 
@@ -55,7 +83,8 @@ namespace StringUtils {
                 }
 
                 returnQuery += $"{{ Found in whole input: {letterInInput}, Found in indices: {letterFound} }} }}";
-            } catch (FormatException err)
+            }
+            catch (FormatException err)
             {
                 Console.WriteLine("Try inputting a single character next time!", err);
             }
