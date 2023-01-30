0faf04888d5bcf0411a853689dcadf05d5c26486
Pluto <112419543+pluto-bohl-student@users.noreply.github.com>
2023-01-30 18:29:00 +0000

    i hate xml comments :3

--- Underloading/ArrayUtils/AddArray.cs
+++ Underloading/ArrayUtils/AddArray.cs
@@ -2,6 +2,19 @@ namespace ArrayUtils
 {
     public class addthemarrays
     {
+        /// <summary>
+        /// <c>AddArray (int)</c> adds two integer arrays together.
+        /// <para>
+        /// Literally just adds two integer arrays together.
+        /// </para>
+        /// <para>
+        /// Requires:
+        /// </para>
+        /// <list type="number">
+        /// <item><description><para><em>Integer array 1.</em></para></description></item>
+        /// <item><description><para><em>Integer array 2.</em></para></description></item>
+        /// </list>
+        /// </summary>
         public static int AddArray(int[] x, int[] y)
         {
             int added = 0;
@@ -15,6 +28,19 @@ namespace ArrayUtils
             return added;
         }
 
+        /// <summary>
+        /// <c>AddArray (int)</c> adds two double arrays together.
+        /// <para>
+        /// Literally just adds two double arrays together.
+        /// </para>
+        /// <para>
+        /// Requires:
+        /// </para>
+        /// <list type="number">
+        /// <item><description><para><em>Double array 1.</em></para></description></item>
+        /// <item><description><para><em>Double array 2.</em></para></description></item>
+        /// </list>
+        /// </summary>
         public static double AddArray(double[] x, double[] y)
         {
             double added = 0;
