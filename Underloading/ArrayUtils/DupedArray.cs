57559367ff9b82b6ad8c69e632188967b3af99e8
Lukas Batema <112420897+lukas-batema-student@users.noreply.github.com>
2023-01-30 12:09:02 -0500

    Add files via upload
    Signed-off-by: Lukas Batema <112420897+lukas-batema-student@users.noreply.github.com>

---
+++ Underloading/ArrayUtils/DupedArray.cs
@@ -0,0 +1,36 @@
+namespace ArrayUtils
+{
+    public class DupedArray
+    {
+        public static string Dupe(int[] x)
+        {
+            Console.Write("Enter how many times would you like to duplicate your array: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int mult = (x.Length + 1) * num;
+
+            int[] y = new int[x.Length];
+
+            string fullString = "";
+
+            int r = 0;
+            for (int i = 0; i < mult; i++)
+            {
+                if (r < x.Length)
+                {
+                    y[r] = x[r];
+                    r++;
+                }
+                else if (r == x.Length)
+                {
+                    r = 0;
+
+                    int[] newerArr = y;
+                    fullString += $"{OutputToString.ConvertToString(newerArr)}\n";
+                }
+            }
+
+            return fullString;
+        }
+    }
+}
---
+++ Underloading/ArrayUtils/OutputToString.cs
@@ -0,0 +1,26 @@
+namespace ArrayUtils
+{
+    public static class OutputToString
+    {
+        public static string ConvertToString(int[] x)
+        {
+            string inputToString = "{ ";
+            int i = 0;
+
+            foreach (int item in x)
+            {
+                if (i < x.Length - 1)
+                {
+                    inputToString += $"{Convert.ToString(x.GetValue(i))}, ";
+                }
+                else
+                {
+                    inputToString += $"{Convert.ToString(x.GetValue(i))} }}";
+                }
+                i++;
+            }
+
+            return inputToString;
+        }
+    }
+}
