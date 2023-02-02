ecfd9d85f2a7b5749486c307a7c4a6b94e4d40f5
Pluto <112419543+pluto-bohl-student@users.noreply.github.com>
2023-02-02 18:21:51 +0000

    :3

--- Underloading/Program.cs
+++ Underloading/Program.cs
@@ -5,14 +5,6 @@ namespace Underloading
 {
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            int[] testArray1 = {1, 4, 18, 7, 9, 53};
-            int[] testArray2 = {2, 5, 19, 8, 10, 54};
-
-            int ans = ArrayUtils.SubArray(testArray1, testArray2);
-
-            Console.WriteLine("The answer is: " + ans);
-        }
+        // Hi :3
     }
 }
